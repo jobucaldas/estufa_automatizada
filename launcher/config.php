eeaaf7cc3ea6385ed10ae81bc9a4effdaae6e325
João Bueno <jobucaldas@users.noreply.github.com>
2018-08-11 22:48:24 -0300

    Added web launcher

---
+++ launcher/config.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+	<title>Arduino</title>
+
+ 	<!-- Inclui as etiquetas -->
+ 	<?php include_once('lib/etiquetasConfig.inc'); ?>
+
+	<!-- Inclui as folhas e arquivos de estilo -->
+	<?php include_once('lib/styleConfig.inc'); ?>
+
+	<!-- Inclui os scripts Javascript -->
+	<?php include_once('lib/scriptConfig.inc'); ?>
+
+</head>
+<body>
+
+	<div class="row">
+		<?php include_once('obj/navbarLateral.inc'); ?>
+
+		<div style="z-index: -1;" class="col-md-3"></div>
+
+		<div class="col-md-9">
+			
+		</div>
+	</div>
+
+</body>
+</html>
